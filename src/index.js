b0c5125c65cf352d375769297fd988445264d862
scott <scottw91@live.ca>
2021-11-02 17:34:36 -0400

    race car in and able to move side to side on road

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import race from './assets/race.png';
+import car from './assets/car.jpeg';
+
+var raceCar
 
 class MyGame extends Phaser.Scene
 {
@@ -12,18 +15,39 @@ class MyGame extends Phaser.Scene
     preload ()
     {
         this.load.image('race', race);
+        this.load.image('car', car)
     }
 
     create ()
     {
-        // const logo = this.add.image(500, 400, 'race');
-        this.tileSprite = this.add.tileSprite(400, 300, 800, 600, 'race')
-        // this.tileSprite.autoScroll(0, 100)
+        this.tileSprite = this.add.tileSprite(400, 300, 850, 800, 'race')
 
+        this.raceCar = this.add.sprite(400, 450, 'car' )
+        this.raceCar.setScale(.05)
+
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update(){
-         this.tileSprite.tilePositionY += 2;
+         this.tileSprite.tilePositionY -= 2;
+
+         const body = this.raceCar.body
+
+
+    if(this.cursors.left.isDown){
+      console.log('up pressed')
+      if(this.raceCar.x > 150){
+      this.raceCar.x -= 10
+      }
+
+    }
+    else if(this.cursors.right.isDown){
+      console.log('down pressed')
+      if(this.raceCar.x < 650){
+        this.raceCar.x += 10
+      }
+
+    }
     }
 }
 
