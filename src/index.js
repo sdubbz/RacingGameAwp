bbaf409f3bbc5d031c057e3c2d1fd774396d7ff5
scott <scottw91@live.ca>
2021-11-02 21:14:54 -0400

    can detect collsion, with greencar sprite, and destroy, on collision

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import Phaser from 'phaser';
+import Phaser, { Physics } from 'phaser';
 import logoImg from './assets/logo.png';
 import race from './assets/race.png';
 import car from './assets/car.jpeg';
+import greenCar from './assets/greenCar.png';
 
 var raceCar
 
@@ -16,6 +17,7 @@ class MyGame extends Phaser.Scene
     {
         this.load.image('race', race);
         this.load.image('car', car)
+        this.load.image('greenCar', greenCar)
     }
 
     create ()
@@ -24,31 +26,66 @@ class MyGame extends Phaser.Scene
 
         this.raceCar = this.add.sprite(400, 450, 'car' )
         this.raceCar.setScale(.05)
+        this.physics.add.existing(this.raceCar, true)
+
+
+
+        this.greenCar = this.add.sprite(400, 250, 'greenCar' )
+        this.greenCar.setScale(.08)
+        this.greenCar.setAngle(-270)
+
+        this.physics.add.existing(this.greenCar)
+
+        // this.raceCar.body.onCollide = new Phaser.Signal();
+        // this.raceCar.body.onCollide.add(this.hitCar, this);
+
+        this.physics.add.collider(this.raceCar, this.greenCar, () => {
+            this.greenCar.destroy()
+            console.log('destroyed')
+
+        })
+
+
+
 
         this.cursors = this.input.keyboard.createCursorKeys()
+
+
     }
 
     update(){
+
          this.tileSprite.tilePositionY -= 2;
 
          const body = this.raceCar.body
 
 
     if(this.cursors.left.isDown){
-      console.log('up pressed')
+
       if(this.raceCar.x > 150){
-      this.raceCar.x -= 10
+      this.raceCar.x -= 10,
+      this.raceCar.body.x -= 10
       }
 
     }
     else if(this.cursors.right.isDown){
-      console.log('down pressed')
+
       if(this.raceCar.x < 650){
         this.raceCar.x += 10
+        this.raceCar.body.x += 10
       }
 
     }
+
+    // this.raceCar1.y += 2;
+
+    if (this.greenCar.y > 565)
+    {
+        this.greenCar.y = -450;
     }
+    }
+
+
 }
 
 const config = {
@@ -56,7 +93,14 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    scene: MyGame,
+    physics: {
+        default: 'arcade',
+        arcade:{
+            gravity: {y: 20},
+            debug: true
+        }
+    }
 };
 
 const game = new Phaser.Game(config);
